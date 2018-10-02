aec883387b9877720d22fad0442529089120c6f6
Gag <xgagik8gmail.com>
2018-10-02 18:33:43 +0400

    added `     .object game from Game class     .obect actualBlock from Blocks class

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -11,11 +11,11 @@ namespace Tetris
             Window window = new Window(60, 50);
             Menu menu = new Menu();
             Admin admin = new Admin();
-            Blocks bigS = new BigS();
             World world = new World();
             Platform platform = new Platform();
-            
-            //List<Dot> d = bigS.Righter(bigS.squares);
+            Game game = new Game();
+
+            Blocks actualBlock = game.RandomBlock();
 
             while (true)
             {
@@ -29,18 +29,18 @@ namespace Tetris
 
                     if(menu.IfStarted())
                     {
-                        if(!bigS.created)
-                        bigS.Create();
+                        if(!actualBlock.created)
+                            actualBlock.Create();
 
-                        if(bigS.moveable)
-                        bigS.Move(bigS.squares, key.Key, world , platform);
+                        if(actualBlock.moveable)
+                            actualBlock.Move(actualBlock.squares, key.Key, world , platform);
                     }
                         
                 }
                 admin.ConsolePosShow(Console.CursorLeft , Console.CursorTop);
 
-                if (menu.IfStarted() && bigS.moveable)
-                    bigS.Gravity(bigS.squares , world , platform);
+                if (menu.IfStarted() && actualBlock.moveable)
+                    actualBlock.Gravity(actualBlock.squares , world , platform);
                 Thread.Sleep(300);
             }
 
