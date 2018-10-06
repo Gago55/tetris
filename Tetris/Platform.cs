6a93b25ee9c9a9c1a28b02842c8aea20e4dd6718
Gag <xgagik8gmail.com>
2018-10-07 01:00:48 +0400

    added `     .method FullPLatform which is detected is line full and if it full it deletes that line.....but there are some BUGS YET

--- Tetris/Platform.cs
+++ Tetris/Platform.cs
@@ -18,5 +18,58 @@ namespace Tetris
                 }
             }
         }
+
+        public void FullPlatform()
+        {
+            List<int> fullY = new List<int>();
+            for (int i = 39; i >10; i-=2)
+            {
+                int count = 0;
+                foreach (Dot d in platform)
+                {
+                    if (d.y == i)
+                        count++;
+                }
+
+                if (count == 30)
+                {
+                    fullY.Add(i);
+                }
+            }
+
+
+            if (fullY.Count == 1)
+            {
+                int y = fullY[0];
+                List<Dot> Removeable = new List<Dot>();
+                foreach(Dot dot in platform)
+                {
+                    if (dot.y == y || dot.y==y-1)
+                    {
+                        dot.Clear();
+                        Removeable.Add(dot);
+                    }
+                }
+                Remove(Removeable);
+
+                foreach (Dot dot in platform)
+                {
+                    if (dot.y < y-1)
+                    {
+                        dot.Clear();
+                        dot.y+=2;
+                        dot.Draw();
+                    }
+                }
+            }
+
+            
+        }
+
+        private void Remove(List<Dot> removeable)
+        {
+            foreach (Dot d in removeable)
+                platform.Remove(d);
+        }
     }
 }
