a922341f015ac1a776d8a9b6eb2775c272da3049
Gag <xgagik8gmail.com>
2018-10-10 00:10:22 +0400

    Color fixes

--- Tetris/UI.cs
+++ Tetris/UI.cs
@@ -8,9 +8,12 @@ namespace Tetris
     {
         public void ShowPlayerStatus(Player player)
         {
+            ConsoleColor color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.White;
             Text(1, 1, "Score:" + player.score.ToString());
             Text(1, 2, "Lines:" + player.lines.ToString());
             Text(1, 3, "Level:" + player.level.ToString());
+            Console.ForegroundColor = color;
         }
     }
 }
