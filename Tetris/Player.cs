1be262a8b5cfc148471185059923240d8b49d67c
Gag <xgagik8gmail.com>
2018-10-09 16:01:12 +0400

    added `     .class Player     .'properties' speed and score     .method AddScore which is adding score

---
+++ Tetris/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tetris
+{
+    class Player
+    {
+        public int speed = 1;
+        public int score = 0;
+
+        public void AddScore(int deltaScore)
+        {
+            score += deltaScore;
+        }
+
+    }
+}
