811145455d97aeef7233cf4f48eb451750957676
Gag <xgagik8gmail.com>
2018-10-09 16:22:30 +0400

    added `     .'property' lines     .method LinesControls which is adding lines count and score

--- Tetris/Player.cs
+++ Tetris/Player.cs
@@ -8,11 +8,26 @@ namespace Tetris
     {
         public int speed = 1;
         public int score = 0;
+        public int lines = 0;
 
         public void AddScore(int deltaScore)
         {
             score += deltaScore;
         }
 
+        public void LinesControls(int count)
+        {
+            lines += count;
+
+            if (count == 1)
+                AddScore(100);
+            else if (count == 2)
+                AddScore(300);
+            else if (count == 3)
+                AddScore(500);
+            else if (count == 4)
+                AddScore(800);
+
+        }
     }
 }
