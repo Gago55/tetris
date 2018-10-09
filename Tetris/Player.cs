07894e507c4ced3561a23ffc18f45b9d0dcba3aa
Gag <xgagik8gmail.com>
2018-10-09 16:45:49 +0400

    added `     .property level     .method ChangePlayerLevel which changed player's level and speed

--- Tetris/Player.cs
+++ Tetris/Player.cs
@@ -9,10 +9,11 @@ namespace Tetris
         public int speed = 1;
         public int score = 0;
         public int lines = 0;
+        public int level = 1;
 
         public void AddScore(int deltaScore)
         {
-            score += deltaScore;
+            score += deltaScore*level;
         }
 
         public void LinesControls(int count)
@@ -28,6 +29,40 @@ namespace Tetris
             else if (count == 4)
                 AddScore(800);
 
+        }
+        
+        public void ChangePlayerLevel()
+        {
+            if (score >= 1000 && score < 2000)
+            {
+                level = 2;
+                speed = 2;
+            }
+            else if (score >= 2000 && score < 3500)
+            {
+                level = 3;
+                speed = 3;
+            }
+            else if (score >= 3500 && score < 5000)
+            {
+                level = 4;
+                speed = 4;
+            }
+            else if (score >= 5000 && score < 8000)
+            {
+                level = 5;
+                speed = 5;
+            }
+            else if (score >= 8000 && score < 15000)
+            {
+                level = 6;
+                speed = 6;
+            }
+            else if (score >= 15000 && score < 20000)
+            {
+                level = 7;
+                speed = 7;
+            }
         }
     }
 }
