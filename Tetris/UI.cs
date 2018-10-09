15fecaf36d4ede1f23b3cd1014df1216362f9d6e
Gag <xgagik8gmail.com>
2018-10-09 16:44:24 +0400

    added comment to show player's level in ShowPLayerStatus method

--- Tetris/UI.cs
+++ Tetris/UI.cs
@@ -10,6 +10,7 @@ namespace Tetris
         {
             Text(1, 1, "Score:" + player.score.ToString());
             Text(1, 2, "Lines:" + player.lines.ToString());
+            Text(1, 3, "Level:" + player.level.ToString());
         }
     }
 }
