fa9d8d8a1089122ac0c24d0c03f9ade939a6997b
Gag <xgagik8gmail.com>
2018-10-09 16:00:20 +0400

    added `     .class UI which is responsible for User Interface     .added method which is showing player's score in top-left corner of window

---
+++ Tetris/UI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tetris 
+{
+    class UI : Instruments
+    {
+        public void ShowScore(Player player)
+        {
+            Text(1, 1, "Score:" + player.score.ToString());
+        }
+    }
+}
